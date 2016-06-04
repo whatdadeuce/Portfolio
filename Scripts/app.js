4ff1f9d5ae837cf9685eb46156dbb67ab63e5588
whatdadeuce <kenneth_bato16@yahoo.com>
2016-06-03 23:53:26 -0400

    Assignment Submission
    -Revise logo/brand on pages
    -Finalize index, Projects and Contact page
    -Add profile photo
    -Add documentation
    -Change placeholder on forms
    -Remove project submenu from app.css
    -Add code to app.js
    -Intergrate js file content to Projects and index page

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,8 +1,49 @@
-/*Main JavaScript File*/
+/**
+ * FileName: app.js
+ * 
+ * @author Kenneth Bato
+ * @date June 3, 2016
+ * 
+ * StudentID: 300448795
+ * website: kenportfolio.azurewebsites.net
+ * @description This file is the main javascript file for the web site
+ */
 
 //IIFE
-(function () {
+(function() {
     "use strict";
+    //create reference for aboutMe
+    var aboutMe = document.getElementById("aboutMe");
 
+    //data for aboutMe. Check to see if aboutMe exists on the page
+    if (aboutMe){
+    aboutMe.innerHTML = "<h2 class='display-1'>About Me</h2>" +
+        "<p class='lead'>Architectural Technology Graduate with" +
+        "five years of experience working for an Architectural Firm and " +
+        "currently in school studying Software Engineering Technology.</p>" +
+        "<hr class='m-y-md'>" +
+        "<p>Passionate, self motivated, ambitious and enjoys learning especially those of personal interest.</p>";
+   }
 
+    //create array reference for projectDesc
+    var projectDesc = [];
+    	projectDesc[0]= document.getElementById("projectDescBakery");
+    	projectDesc[1]= document.getElementById("projectDescDairy");
+    	projectDesc[2]= document.getElementById("projectDescProduce");
+
+	//date from projectDesc
+    var DescArray = [];
+    	DescArray[0] = "<div class='panel'><h1>Grocery Interior - Bakery Department</h1>"+
+                                    "<p>Interior rendering of proposed Bakery department design</p></div>";
+    	DescArray[1] = "<div class='panel'><h1>Grocery Interior - Dairy Department</h1>"+
+                                    "<p>Interior rendering of proposed Dairy department design</p></div>";
+    	DescArray[2] = "<div class='panel'><h1>Grocery Interior - Produce Department</h1>"+
+                                    "<p>Interior rendering of proposed Produce department design</p></div>";
+
+    //iterate and check if projectDesc reference id exists on the page and apply appropriate data
+    for (var i = projectDesc.length; i >=0; i--){
+    	if (projectDesc[i]){
+    		projectDesc[i].innerHTML = DescArray[i];
+    	}
+    }
 })();
